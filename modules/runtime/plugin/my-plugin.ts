30c804ad7a4ff42ef928b4f100f5bc7bbe981848
Hebilicious <xsh4k3@gmail.com>
2023-08-10 22:05:04 +0700

    1 file modified, 1 added, 2 deleted

--- modules/runtime/plugin/my-plugin.ts
+++ modules/runtime/plugin/my-plugin.ts
@@ -1,4 +1,4 @@
-import testMiddleware from "../middlewares/test"
+import testMiddleware from "../middleware/test"
 
 export default defineNuxtPlugin(()=> {
   console.log("Hello from my module plugin")
